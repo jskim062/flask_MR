ffe3f580fccc710e61934b783864dc6947c5371a
jskim062 <jjjskim062@gmail.com>
2025-12-13 17:55:24 +0900

    for test

--- tests/test_flask_MR.py
+++ tests/test_flask_MR.py
@@ -1,5 +1,8 @@
 import pytest
-from flask_MR.py import app
+from flask_MR import app
+import sys  
+import os  
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  
 
 
 @pytest.fixture
