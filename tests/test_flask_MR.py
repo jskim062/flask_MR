75ccf493e9d0f9c807098b23a2d47e8ec66ad8e2
jskim062 <jjjskim062@gmail.com>
2025-12-13 20:03:49 +0900

    I Changed output string from 'hello' to 'hi'

--- tests/test_flask_MR.py
+++ tests/test_flask_MR.py
@@ -1,10 +1,9 @@
-
-import flask_MR1
-from flask_MR1 import app
-import pytest
 import sys
 import os
+import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import flask_MR1
+from flask_MR1 import app
 
 
 @pytest.fixture
